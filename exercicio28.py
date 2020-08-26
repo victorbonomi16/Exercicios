12ca15ffa68e385919d91b4d2ab4728c9b9a65fe
victorbonomi16 <bononikis@gmail.com>
2020-08-26 19:24:20 -0300

    Exercicio 28 concluido

---
+++ exercicio28.py
@@ -0,0 +1,7 @@
+n1 = int (input("informe o primeiro numero  :" ))
+n2 = int (input("informe o segundo numero :" ))
+n3 = int (input("informe o terceiro numero  :" ))
+
+total=(n1**2)+(n2**2)+(n3**2) 
+
+print("a soma dos quadrados dos numeros informados é : ",total) 
