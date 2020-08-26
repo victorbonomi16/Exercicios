4b39be0a688c898ad5d127a228f51ed07b0b9a2c
victorbonomi16 <bononikis@gmail.com>
2020-08-26 16:42:22 -0300

    Exercicio 1 e 2 feitos

---
+++ exercicio1e2.py
@@ -0,0 +1,8 @@
+#exercicio 1
+n1 = int (input("informe um numero inteiro:" ))
+print("o valor digitado " , (n1))
+ 
+#exercicio 2 
+n2 = float (input("informe um numero real:" ))
+print("o valor digitado " , (n2)) 
+ 
