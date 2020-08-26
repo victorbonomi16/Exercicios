e04dcca11d8f7a6f98e1f7f50c5be841b5b55b1b
victorbonomi16 <bononikis@gmail.com>
2020-08-26 16:49:09 -0300

    Exercicio 3 feito

---
+++ exercicio3.py
@@ -0,0 +1,8 @@
+ 
+n1 = int (input("informe o primeiro numero inteiro:" ))
+n2 = int (input("informe o segundo numero inteiro:" ))
+n3 = int (input("informe o terceiro numero inteiro:" ))
+
+print("a soma dos numeros informados é : ",(n1+n2+n3)) 
+ 
+ 
